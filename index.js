8e3421344a47bcdd941ddea174df4932e3970f47
Nick Warner <nickwarner@gmail.com>
2019-01-11 12:48:41 -0500

    Optionally update a m3u file to include the download file

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const fetch = require('node-fetch')
 const fs = require('fs')
 
-// Usage: node index.js [optionalPathToDownload]
+// Usage: node index.js [optionalPathToDownload] [optionalPathToM3UFileToAppend]
 
-let customPath = process.argv.slice(2)
+let args = process.argv.slice()
+let customPath = args[2]
+let playlistPath = args[3]
 
 const run = async () => {
   var prevSaturday = new Date()
@@ -12,16 +14,25 @@ const run = async () => {
   let timestamp = dateString + 'T17:00:00Z'
 
   let downloadPath = customPath || './downloads'
-  downloadPath = downloadPath + '/positive-vibrations-' + dateString + '.mp3'
+  let filename = 'positive-vibrations-' + dateString + '.mp3'
+  let fullPath = downloadPath + '/' + filename
 
   let data = await fetch(`https://legacy-api.kexp.org/get_streaming_url?bitrate=256&timestamp=${timestamp}`).then((response) => {
     return response.json()
   })
 
   if (data && data['sg-url']) {
-    console.log('==> Downloading Positive Vibrations ' + dateString + ' to ' + downloadPath)
+    console.log('==> Downloading Positive Vibrations ' + dateString + ' to ' + fullPath)
     let response = await fetch(data['sg-url'])
-    response.body.pipe(fs.createWriteStream(downloadPath))
+    let writeStream = fs.createWriteStream(fullPath)
+    writeStream.on('close', () => {
+      console.log('==> Done!')
+      if (playlistPath) {
+        fs.appendFileSync(playlistPath, '\n' + filename, 'utf8')
+        console.log('==> Saved to playlist: ' + playlistPath)
+      }
+    })
+    response.body.pipe(writeStream)
   } else {
     console.log(':( Download error - ' + dateString)
   }
