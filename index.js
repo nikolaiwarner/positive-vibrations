816f145b94cfe613f409ced28a31492d69e4b2c2
nikolaiwarner <n@nikolaiwarner.com>
2019-02-10 16:54:11 -0500

    Add cli options to fetch for any time

--- index.js
+++ index.js
@@ -1,35 +1,60 @@
 const fetch = require('node-fetch')
 const fs = require('fs')
+const path = require('path')
+const minimist = require('minimist')
 
-// Usage: node index.js [optionalPathToDownload] [optionalPathToM3UFileToAppend]
+var args = minimist(process.argv.slice(2))
 
-let args = process.argv.slice()
-let customPath = args[2]
-let playlistPath = args[3]
+var usage = `
+  Usage: node index.js [options]
+
+  Options:
+    --dir           Path to download file || ./downloads
+    --weekday       Number of day of week to fetch || 1 (Saturday)
+    --hour          Hour of day in UTC to fetch || 17 (9am PST)
+    --title         Show title for filename || 'positive-vibrations'
+    --playlistPath  Optional path to M3U file to append
+    --version       Print out which version is running
+    --help          Print this help message
+`
+
+if (args.version || args.v) {
+  console.log(JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8')).version)
+  process.exit(0)
+}
+
+if (args.help || args.h) {
+  process.stderr.write(usage)
+  process.exit(1)
+}
 
 const run = async () => {
-  var prevSaturday = new Date()
-  prevSaturday.setDate(prevSaturday.getDate() - (prevSaturday.getDay() + 1) % 7)
-  let dateString = prevSaturday.toISOString().split('T')[0]
-  let timestamp = dateString + 'T17:00:00Z'
+  let weekday = args.weekday || 1
+  let hour = args.hour || 17
+  let title = args.title || 'positive-vibrations'
 
-  let downloadPath = customPath || './downloads'
-  let filename = 'positive-vibrations-' + dateString + '.mp3'
-  let fullPath = downloadPath + '/' + filename
+  var prevWeekday = new Date()
+  prevWeekday.setDate(prevWeekday.getDate() - (prevWeekday.getDay() + weekday) % 7)
+  let dateString = prevWeekday.toISOString().split('T')[0]
+  let timestamp = dateString + 'T' + hour + ':00:00Z'
 
   let data = await fetch(`https://legacy-api.kexp.org/get_streaming_url?bitrate=256&timestamp=${timestamp}`).then((response) => {
     return response.json()
   })
 
   if (data && data['sg-url']) {
-    console.log('==> Downloading Positive Vibrations ' + dateString + ' to ' + fullPath)
+    let downloadPath = args.dir || './downloads'
+    let filename = title + '-' + dateString + '.mp3'
+    let fullPath = downloadPath + '/' + filename
+
+    console.log('==> Downloading ' + title + ' ' + dateString + ' to ' + fullPath)
     let response = await fetch(data['sg-url'])
     let writeStream = fs.createWriteStream(fullPath)
     writeStream.on('close', () => {
       console.log('==> Done!')
-      if (playlistPath) {
-        fs.appendFileSync(playlistPath, '\n' + filename, 'utf8')
-        console.log('==> Saved to playlist: ' + playlistPath)
+      if (args.playlistPath) {
+        fs.appendFileSync(args.playlistPath, '\n' + filename, 'utf8')
+        console.log('==> Saved to playlist: ' + args.playlistPath)
       }
     })
     response.body.pipe(writeStream)
